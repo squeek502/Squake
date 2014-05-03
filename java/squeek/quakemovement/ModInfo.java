7b25941e450898d2bda4fb22c7d7cbbafe1ef9a9
squeek <squeek502@hotmail.com>
2014-05-02 21:31:44 -0700

    Moved versioning stuff into a separate java file so that dollar sign characters in other source files don't conflict with the gradle replace task

---
+++ java/squeek/quakemovement/ModInfo.java
@@ -0,0 +1,7 @@
+package squeek.quakemovement;
+
+public final class ModInfo
+{
+	public static final String MODID = "squeek_QuakeMovement";
+	public static final String VERSION = "${version}";
+}
--- java/squeek/quakemovement/ModQuakeMovement.java
+++ java/squeek/quakemovement/ModQuakeMovement.java
@@ -1,5 +1,6 @@
 package squeek.quakemovement;
 
+import squeek.quakemovement.ModInfo;
 import api.player.client.ClientPlayerAPI;
 import api.player.server.ServerPlayerAPI;
 import cpw.mods.fml.common.Mod;
@@ -11,12 +12,12 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid="squeek_QuakeMovement", name="Quake Movement", version="0.1.0", dependencies="required-after:PlayerAPI")
+@Mod(modid=ModInfo.MODID, name="Quake Movement", version=ModInfo.VERSION, dependencies="required-after:PlayerAPI")
 @NetworkMod(clientSideRequired=true)
 public class ModQuakeMovement {
 
         // The instance of your mod that Forge uses.
-        @Instance(value = "squeek_QuakeMovement")
+        @Instance(value = ModInfo.MODID)
         public static ModQuakeMovement instance;
        
         @EventHandler
@@ -30,9 +31,9 @@ public class ModQuakeMovement {
        
         @EventHandler
         public void postInit(FMLPostInitializationEvent event) {
-    		ServerPlayerAPI.register("squeek_QuakeMovement", QuakeServerPlayer.class);
+    		ServerPlayerAPI.register(ModInfo.MODID, QuakeServerPlayer.class);
     		
         	if (event.getSide() == Side.CLIENT)
-            	ClientPlayerAPI.register("squeek_QuakeMovement", QuakeClientPlayer.class);
+            	ClientPlayerAPI.register(ModInfo.MODID, QuakeClientPlayer.class);
         }
 }
