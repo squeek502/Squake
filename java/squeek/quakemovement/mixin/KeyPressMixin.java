b4667951c211e5bb4fbec9d8b246df449add686b
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-08-01 21:30:34 -0500

    make toggling use the action bar+change lang files

--- java/squeek/quakemovement/mixin/KeyPressMixin.java
+++ java/squeek/quakemovement/mixin/KeyPressMixin.java
@@ -1,27 +1,23 @@
 package squeek.quakemovement.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.resource.language.I18n;
+import net.minecraft.text.TranslatableText;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import squeek.quakemovement.KeyBindInitializer;
-import squeek.quakemovement.ModInfo;
 import squeek.quakemovement.ModQuakeMovement;
 
 @Mixin(MinecraftClient.class)
 public class KeyPressMixin
 {
 	@Inject(method = "handleInputEvents", at=@At("HEAD"))
-	public void handleInputEvents(CallbackInfo info)
-	{
-		if (KeyBindInitializer.ENABLE.wasPressed())
-		{
+	public void handleInputEvents(CallbackInfo info) {
+		if (KeyBindInitializer.ENABLE.wasPressed()) {
 			ModQuakeMovement.CONFIG.setEnabled(!ModQuakeMovement.CONFIG.isEnabled());
-
-			String feedback = ModQuakeMovement.CONFIG.isEnabled() ? I18n.translate("squake.key.toggle.enabled") : I18n.translate("squake.key.toggle.disabled");
-			MinecraftClient.getInstance().player.sendChatMessage("[" + ModInfo.MODID + "] " + feedback);
+			MinecraftClient.getInstance().player.sendMessage(ModQuakeMovement.CONFIG.isEnabled() ? new TranslatableText("squake.key.toggle.enabled") : new TranslatableText("squake.key.toggle.disabled"), true);
 		}
 	}
 }
