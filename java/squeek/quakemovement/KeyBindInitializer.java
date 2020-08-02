4e396d2897a30ad37b96e72cf4d9e4c7ce405854
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-08-01 20:59:19 -0500

    update to new fabric keybinding api and rebrand

--- java/squeek/quakemovement/KeyBindInitializer.java
+++ java/squeek/quakemovement/KeyBindInitializer.java
@@ -3,30 +3,18 @@ package squeek.quakemovement;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
-public class KeyBindInitializer implements ClientModInitializer
-{
-	public static FabricKeyBinding ENABLE;
-	public static String CATEGORY = "fabric.mods." + ModInfo.MODID;
+public class KeyBindInitializer implements ClientModInitializer {
+	public static final String CATEGORY = "fabric.mods." + ModInfo.MODID;
+	public static final KeyBinding ENABLE = new KeyBinding(CATEGORY + "." + "enable", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, CATEGORY);
 
 	@Override
-	public void onInitializeClient()
-	{
-		KeyBindingRegistry.INSTANCE.addCategory(CATEGORY);
-		ENABLE = registerKey("enable", GLFW.GLFW_KEY_UNKNOWN, CATEGORY);
-	}
-
-	public static FabricKeyBinding registerKey(String name, Integer code, String category)
-	{
-		FabricKeyBinding key = FabricKeyBinding.Builder.create(new Identifier(ModInfo.MODID, name), InputUtil.Type.KEYSYM, code, category).build();
-		KeyBindingRegistry.INSTANCE.register(key);
-
-		return key;
+	public void onInitializeClient() {
+		KeyBindingHelper.registerKeyBinding(ENABLE);
 	}
 }
